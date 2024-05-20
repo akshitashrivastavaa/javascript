cbc8a7ab1973495b094309d91fd44b095e623827
akshitashrivastavaa <125906350+akshitashrivastavaa@users.noreply.github.com>
2024-05-20 07:23:30 +0000

    datatypes

---
+++ 01_basics/1_variable.js
@@ -0,0 +1,15 @@
+const account_id=1122 //cannot be changed
+let email="akshitashri4@gmail.com"
+var apass="1234"
+
+/*var is prefered not to useed 
+because of the issue in block scope and 
+functional space*/
+
+acc_addr="jaipur"
+
+console.log(email)
+console.log( account_id);
+email="aks@gmail.com"
+console.log(email,apass)
+console.table([apass,email])
---
+++ 01_basics/2_variable.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all code as newer js
+
+//alert(2+2) //we are using nodejs not browser
+console.log(2+2);
+
+//primitive
+//number=>2 to power 53
+//string=""
+//bigint
+//boolean=>true or false
+let a;//undefined
+let age=21;
+let m=null;//null=>stand alone value
+//symbol=> unique
+
+//object
+
+console.log(typeof a);
+console.log(typeof 21);
+console.log(typeof null);//object will be displayed
+console.log(typeof undefined);//undefined
